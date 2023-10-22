e279c367347856b313be057a3caae75d22c5e844
Broc Keeton <broc.keeton@student.centralia.edu>
2023-10-22 12:51:16 -0700

    Added a administrator and faculty component. Then routed them to the nav bar. (may change this to route from buttons on landing page)

---
+++ SurveyApp/ClientApp/src/app/faculty/faculty.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faculty',
+  templateUrl: './faculty.component.html',
+  styleUrls: ['./faculty.component.css']
+})
+export class FacultyComponent {
+
+}
