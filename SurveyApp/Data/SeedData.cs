ce0365c7333e48466647274853fce118ddd87c5d
Broc Keeton <broc.keeton@student.centralia.edu>
2023-10-30 16:40:31 -0700

    Began work on SeedData.cs. Added structure for data.

--- SurveyApp/Data/SeedData.cs
+++ SurveyApp/Data/SeedData.cs
@@ -1,6 +1,47 @@
-﻿namespace SurveyApp.Data
+﻿using Microsoft.AspNetCore.Identity;
+using SurveyApp.Models;
+
+namespace SurveyApp.Data
 {
     public class SeedData
     {
+        public static async Task EnsurePopulated(IServiceProvider services)
+        {
+            ApplicationDbContext context = services.GetService<ApplicationDbContext>();
+            IConfiguration configuration = services.GetService<IConfiguration>();
+
+            if (context == null)
+            {
+                throw new NullReferenceException("No context available");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Questionaire seed = new Questionaire
+                {
+
+                    Questions = new List<Question>
+                    {
+                        new Question
+                        {
+                            Responses = new List<Response>
+                            {
+                                new Response
+                                {
+
+                                }
+                            }
+                        }
+                    }
+
+                };
+
+                context.Questionaire.Add(seed);
+
+                await context.SaveChangesAsync();
+
+            }
+
+        }
     }
 }
