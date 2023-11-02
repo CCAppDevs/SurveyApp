ebcd3e9d940c1427b1fc84d16d14b14d8c85c3bf
Jesse Harlan <jesse.harlan@centralia.edu>
2023-11-02 12:02:45 -0700

    Editing edit form to be about questionaires and handling reactive forms

--- SurveyApp/ClientApp/src/app/form/edit-form/edit-form.component.ts
+++ SurveyApp/ClientApp/src/app/form/edit-form/edit-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-form',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class EditFormComponent {
 
+  questionaireForm: FormGroup;
+  id = 1234;
+
+  constructor(private fb: FormBuilder) {
+    this.questionaireForm = this.fb.group({
+      questionaireID: 1234,
+      title: "test this form",
+      createdDate: "2023-04-14",
+      modifiedDate: "2023-04-14",
+      questions: [
+        { questionID: 1, questionText: "test", questionType: 1 },
+        { questionID: 2, questionText: "test 1", questionType: 1 },
+        { questionID: 3, questionText: "test 2", questionType: 1 },
+      ]
+    });
+  }
+
+  addNewQuestion() {
+    // this fires when the add new button is clicked
+    console.log("add new question fired");
+  }
 }
