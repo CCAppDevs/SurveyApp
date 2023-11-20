cb4871733726102739d475c6dbf8ed917356ea8b
Jesse Harlan <jesse.harlan@centralia.edu>
2023-11-20 12:32:40 -0800

    working creation form

--- SurveyApp/ClientApp/src/app/form/edit-form/edit-form.component.ts
+++ SurveyApp/ClientApp/src/app/form/edit-form/edit-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { DataService, Survey } from '../../data.service';
 
 @Component({
   selector: 'app-edit-form',
@@ -18,7 +19,7 @@ export class EditFormComponent {
     'Text'
   ];
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private data: DataService) {
     this.questionaireForm = this.fb.group({
       questionaireID: 1234,
       title: "test form",
@@ -34,8 +35,9 @@ export class EditFormComponent {
 
     // add a new question into the array
     this.questions.push(this.fb.group({
-      questionText: '',
-      questionType: 0
+      prompt: '',
+      promptType: 0,
+      responses: this.fb.array([])
     }));
   }
 
@@ -45,5 +47,17 @@ export class EditFormComponent {
 
   onSubmit(): void {
     console.log("form was submitted", this.questionaireForm.value);
+
+    let mySurvey: Survey = {
+      formId: this.questionaireForm.value.formId,
+      createdDate: this.questionaireForm.value.createdDate,
+      modifiedDate: this.questionaireForm.value.modifiedDate,
+      title: this.questionaireForm.value.title,
+      questions: this.questionaireForm.value.questions
+    }
+
+    this.data.createNewQuestionaire(mySurvey).subscribe(result => {
+      console.log('result of new creation', result);
+    })
   }
 }
